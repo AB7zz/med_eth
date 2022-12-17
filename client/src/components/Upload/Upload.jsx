6e94ea54cad11ba2cae0e9d6b0dabadc21014bc7
ab7z <abhiksi613@gmail.com>
2022-12-18 00:29:28 +0530

    some changes

--- client/src/components/Upload/Upload.jsx
+++ client/src/components/Upload/Upload.jsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import {create as ipfsHttpClient} from 'ipfs-http-client'
+import { DataContext } from '../../DataContext'
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 const Upload = () => {
   localStorage.setItem('doctor', 1)
   const doctor = localStorage.getItem('doctor')
   const [pdf, setPDF] = useState()
+  const {account} = useContext(DataContext)
+  console.log(account)
+
   
+
   const uploadToIPFS = async (event) => {
     event.preventDefault()
     const file = event.target.files[0]
@@ -32,20 +37,20 @@ const Upload = () => {
     }
   }
 
-  const mintPDF = async(results) => {
+  const mintPDF = async(result) => {
     const uri = `https://ipfs.infura.io/ipfs/${result.path}`
     await (await pdf.mint(uri)).wait()
     const id = pdf.tokenCount()
     await (await pdf.setApprovalForAll(database.address, true)).wait()
 
-    await (await database.uploadRecord(pdf.address, '0xf50d19D3514C99185Ecd7B7feA60583fC33A2E69', id, doctor))
-
+    await (await database.uploadRecord(pdf.address, '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', id, doctor))
   }
   return (
     <>
       <div>Upload</div>
-      <input type="file" onChange={uploadToIPFS} name="file" id="" />
-      <button onClick={createPDF}></button>
+      {/* <input type="file" onChange={uploadToIPFS} name="file" id="" /> */}
+      <input type="text" onChange={e=>setMessage(e.value)} placeholder='text' name='pdf' />
+      <button onClick={createPDF}>Upload</button>
     </>
   )
 }
